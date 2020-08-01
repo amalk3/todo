0aa182f7b8db7a2ccedbae5641b911e5afc59b34
amalk3 <amalkottayia3@gmail.com>
2020-08-01 20:50:15 +0530

    Initial commit

---
+++ build/precache-manifest.c9be76a9a6cde3cc2037cde122fade8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72b8a1b618002c213b9a586cacfd3293",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df97a0bf7b98354324da",
+    "url": "/static/css/main.8f26db7e.chunk.css"
+  },
+  {
+    "revision": "ad2aba1752eeb3af78f6",
+    "url": "/static/js/2.b48f9762.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b48f9762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df97a0bf7b98354324da",
+    "url": "/static/js/main.44ed3b24.chunk.js"
+  },
+  {
+    "revision": "559ff28579f192bdf8b9",
+    "url": "/static/js/runtime-main.98bec8e4.js"
+  }
+]);
